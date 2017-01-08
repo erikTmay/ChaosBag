71a50809087aaedb0e90776845d450e87cfffb0a
Erik May <may.erik@gmail.com>
2017-01-07 23:32:28 -0800

    basic unit test start for ChaosBag.jsx

--- src/components/chaosBag/ChaosBag.test.js
+++ src/components/chaosBag/ChaosBag.test.js
@@ -1,6 +1,23 @@
 import React from 'react';
+import ReactTestUtils from 'react-addons-test-utils';
 import ChaosBag from './ChaosBag';
+import chaosBagDefaults from '../../chaosBagDefaults';
+import constants from '../../constants';
 
-it('', () => {
-    expect(true).toEqual(true);
+describe('Chaos Bag', () => {
+    let renderer;
+    const tokens = chaosBagDefaults[constants.SCENARIOS.THE_GATHERING][constants.DIFFICULTIES.STANDARD];
+    const onTokenAmountChange = () => {};
+
+	beforeEach(function() {
+		renderer = ReactTestUtils.createRenderer();
+	});
+
+    it('Should render self.', () => {
+        const chaosBag = renderer.render(<ChaosBag
+            tokens={tokens}
+            onTokenAmountChange={() => onTokenAmountChange()}
+        />);
+        expect(chaosBag).toBeDefined();
+    });
 });
