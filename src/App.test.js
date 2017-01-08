c1bedc520efc202317890cdf3ad9a2c38521afbf
Erik May <may.erik@gmail.com>
2017-01-07 23:05:19 -0800

    adding more unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import ReactTestUtils from 'react-addons-test-utils';
+import ReduxMockStore from 'redux-mock-store';
+import initialState from './reducers/initialState';
 import App from './App';
 
-it('renders without crashing', () => {
-  // const div = document.createElement('div');
-  // ReactDOM.render(<App />, div);
+describe('App', () => {
+    const mockStore = ReduxMockStore();
+	let connectedApp;
+    let store;
+
+	beforeEach(function() {
+		store = mockStore(initialState);
+	});
+
+   describe('renders without crashing', function() {
+		beforeEach(function() {
+			connectedApp = ReactTestUtils.renderIntoDocument(<Provider store={store}><App/></Provider>);
+		});
+
+        it('Selectors', () => {
+            const selectorsWrapper = ReactTestUtils.findRenderedDOMComponentWithClass(connectedApp, 'selectors');
+            expect(selectorsWrapper).toBeDefined();
+        });
+
+        it('Selector', () => {
+            const selectors = ReactTestUtils.scryRenderedDOMComponentsWithClass(connectedApp, 'selector');
+            expect(selectors.length).toEqual(3);
+        });
+
+        it('ChaosBag', () => {
+            const ChaosBag = ReactTestUtils.findRenderedDOMComponentWithClass(connectedApp, 'chaos-bag');
+            expect(ChaosBag).toBeDefined();
+        });
+
+        it('Tokens', () => {
+            const tokens = ReactTestUtils.scryRenderedDOMComponentsWithClass(connectedApp, 'token');
+            expect(tokens.length).toEqual(16);
+        });
+
+        it('TokenPuller', () => {
+            const tokenPuller = ReactTestUtils.findRenderedDOMComponentWithClass(connectedApp, 'tokenPuller');
+            expect(tokenPuller).toBeDefined();
+        });
+	});
 });
